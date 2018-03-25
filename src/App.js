9cb513d845339ed2acc49c4ee4a045bb78db213b
Jokerwolf <jgarm@yandex.ru>
2018-03-25 16:59:47 +0300

    Added state initialization with moment js dates.

--- src/App.js
+++ src/App.js
@@ -8,70 +8,74 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            months: [
-                { name: 'JAN', days: [
-                    { date: '01', tags: [{type: 0}] },
-                    { date: '02', tags: [{type: 1}] },
-                    { date: '03' },
-                    { date: '04' },
-                    { date: '05' },
-                    { date: '06' },
-                    { date: '07' },
-                    { date: '08', tags: [{type: 2}] },
-                    { date: '09', tags: [{type: 1}] },
-                    { date: '10' },
-                    { date: '11', tags: [{type: 3}] },
-                    { date: '12' },
-                    { date: '13' },
-                    { date: '14' },
-                    { date: '15' },
-                    { date: '16' },
-                    { date: '17' },
-                    { date: '18' },
-                    { date: '19' },
-                    { date: '20' },
-                    { date: '21' },
-                    { date: '22' },
-                    { date: '23' },
-                    { date: '24' },
-                    { date: '25' },
-                    { date: '26' },
-                    { date: '27' },
-                    { date: '28' },
-                    { date: '29' },
-                    { date: '30' },
-                    { date: '31' }
-                ] },
-                { name: 'FEB', days: [{ date: '01' }, { date: '02' }, { date: '03', tags: [{type: 2}] }, { date: '04' }, { date: '05' }, { date: '06' }, { date: '07' }] },
-                { name: 'MAR', days: [{ date: '01', tags: [{type: 1}]}, { date: '02' }, { date: '03' }, { date: '04' }, { date: '05' }, { date: '06' }, { date: '07' }] },
-                { name: 'APR', days: [{ date: '01' }, { date: '02' }, { date: '03' }, { date: '04' }, { date: '05' }, { date: '06', tags: [{type: 3}]}, { date: '07' }] }
-            ]
-        };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      months: this.getMonths()
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PopupsContainer show={this.state.show} close={() => this.closePopup()}/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Calendar {...this.state} showPopup={() => this.showPopup()}/>
+      </div>
+    );
+  }
+
+  showPopup() {
+    this.setState({show: true})
+  }
+
+  closePopup() {
+    this.setState({show: false})
+  }
+
+  /****************************************************************************
+  * State initiation
+  ****************************************************************************/
+  getMonthName = (monthNum) => {
+    return moment().month(monthNum).format('MMM')
+  };
+
+  getMonths = () => {
+    const months = new Array(12).fill(1);
+    return months.map((_, ind) => ({name: this.getMonthName(ind), days: this.getMonthDays(ind).map(this.mapDay)}));
+  };
+
+  getMonthDays = (monthNum) => {
+    const month = moment().month(monthNum);
+
+    const start = month.clone().startOf('month');
+    const lengthInDays = month.clone().daysInMonth();
+
+    const days = Array(month.daysInMonth());
+    for (let i = 0, day = start.clone(); i <= lengthInDays - 1; i++) {
+      days[i] = day.clone().add(i, 'day');
     }
 
-    render() {
-        return (
-            <div className="App">
-                <PopupsContainer show={this.state.show} close={() => this.closePopup()}/>
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <Calendar {...this.state} showPopup={() => this.showPopup()}/>
-            </div>
-        );
-    }
+    return days;
+  }
 
-    showPopup() {
-        this.setState({show: true})
-    }
+  mapDay = (day) => ({
+    date: day.date(),
+    tags: [{type: this.getRandomType()}]
+  });
 
-    closePopup() {
-        this.setState({show: false})
-    }
+  getRandomType = () => {
+    const integer = this.getRandomInt(30);
+
+    return integer > 3 ? undefined : integer;
+  };
+
+  getRandomInt = (max) =>
+    Math.floor(Math.random() * Math.floor(max));
 }
 
 export default App;
